c3ca64f0d991be2891c91936e608b01a7b35dea2
jeffrothwell <rothwelljeff@gmail.com>
2018-01-26 14:28:19 -0500

    complete exercise

---
+++ lowercase.rb
@@ -0,0 +1,13 @@
+words_array = ["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
+
+words_array.each do |word|
+  if word.downcase == word && word.length > 4
+    puts "long and lowercase"
+  elsif word.downcase == word
+    puts "lowercase"
+  elsif word.length > 4
+    puts "long"
+  else
+    puts word
+  end
+end
